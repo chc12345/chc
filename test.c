c686a28445cd587804450fa4b562a93cb6bdd478
chc12345 <chc0210@foxmail.com>
2025-10-23 11:41:22 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,12 @@
+//202510125125
+//chc0210@foxmail.com
+//陈惠聪
+#include <stdio.h>
+int main()
+{
+    float a,b;
+    scanf("%f",&a);
+    b = a*9/5 + 32;
+    printf("%.1f",b);
+    return 0;
+}
---
+++ triangle_check.c
@@ -0,0 +1,15 @@
+//202010125125
+//chc0210@foxmail.com
+//陈惠聪
+#include <stdio.h>
+int main()
+{
+    int a , b , c ;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b>c && a+c>b && b+c>a){
+        printf("可以组成三角形");
+    }else{
+        printf("不能组成三角形");
+    }
+    return 0;
+}
