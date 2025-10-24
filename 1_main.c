db855d65d9c6fa12701e95c961595c8d5f4037b8
chc12345 <chc0210@foxmail.com>
2025-10-24 23:53:34 +0800

    4_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,26 +1,15 @@
-//202510125125
+//202010125125
 //chc0210@foxmail.com
 //陈惠聪
 #include <stdio.h>
 int main()
 {
-    int score;
-    printf("请输入一个成绩\n");
-    scanf("%d",&score);
-    if (score >=90 && score <=100){
-        printf("A");
-    }
-    else if (score >=80 && score <=89){
-        printf("B");
-    }
-    else if (score >=70 && score <=79){
-        printf("C");
-    }
-    else if (score >=60 && score <=69){
-        printf("D");
-    }
-    else {
-        printf("E");
+    int a , b , c ;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b>c && a+c>b && b+c>a){
+        printf("可以组成三角形");
+    }else{
+        printf("不能组成三角形");
     }
     return 0;
 }
