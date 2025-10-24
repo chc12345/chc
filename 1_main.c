5d6ab5fb69a47ce5404ad9539a5a0492c86dd119
chc12345 <chc0210@foxmail.com>
2025-10-25 00:13:04 +0800

    Update 2_main.c

--- 1_main.c
+++ 1_main.c
@@ -10,13 +10,13 @@ int main()
     if (score >=90 && score <=100){
         printf("A");
     }
-    else if (score >=80 && score <=89){
+    else if (score >=80 && score <=90){
         printf("B");
     }
-    else if (score >=70 && score <=79){
+    else if (score >=70 && score <=80){
         printf("C");
     }
-    else if (score >=60 && score <=69){
+    else if (score >=60 && score <=70){
         printf("D");
     }
     else {
