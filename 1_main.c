d0ae11bc2e839ec522f76155dbe2056412efa604
chc12345 <chc0210@foxmail.com>
2025-10-25 00:05:38 +0800

    Update 2_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,9 +4,23 @@
 #include <stdio.h>
 int main()
 {
-    float a,b;
-    scanf("%f",&a);
-    b = a*9/5 + 32;
-    printf("%.1f",b);
+    int score;
+    printf("请输入一个成绩\n");
+    scanf("%d",&score);
+    if (score >=90 && score <=100){
+        printf("A");
+    }
+    else if (score >=80 && score <=89){
+        printf("B");
+    }
+    else if (score >=70 && score <=79){
+        printf("C");
+    }
+    else if (score >=60 && score <=69){
+        printf("D");
+    }
+    else {
+        printf("E");
+    }
     return 0;
 }
