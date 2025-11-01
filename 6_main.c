2b4dfea89f7176a8b67c81a8fc9d320bce19684e
chc12345 <chc0210@foxmail.com>
2025-11-01 15:50:29 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,22 +2,28 @@
 //chc0210@foxmailcom
 //陈惠聪
 #include <stdio.h>
-int main()
+
+int main() 
 {
     int arr[5];
     int count = 0;
-    // 读取 5 个整数
+
     for (int i = 0; i < 5; i++) 
     {
         int num;
+        printf("请输入第 %d 个数: ", i + 1);
         scanf("%d", &num);
-        // 如果是偶数，存入数组
-        if (num % 2 == 0) 
-        {
-            arr[count] = num;
-            count++;
+
+        // 如果是奇数，提示重新输入
+        while (num % 2 != 0) {
+            printf("请输入偶数: ");
+            scanf("%d", &num);
         }
+
+        arr[count] = num;
+        count++;
     }
+
     // 输出结果
     for (int i = 0; i < count; i++) 
     {
@@ -28,5 +34,6 @@ int main()
         }
     }
     printf("\n");
+
     return 0;
 }
